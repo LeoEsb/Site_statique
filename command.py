643095cb568792c1cef7b94947db7db292bf057c
LeoEsb <43401721+LeoEsb@users.noreply.github.com>
2019-02-11 19:30:22 +0100

    Update command.py

--- command.py
+++ command.py
@@ -1,11 +1,28 @@
-import click
+import os
+import argparse
+import markdown2
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Quel est ton nom', help='The person to greet.')
-def hello(count, name):
-    for x in range(count):
-        click.echo('Bonjour %s!' % name)
 
-if __name__ == '__main__':
-    hello()
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--input', type = str, required = True, metavar = '', help = 'dossier avec des fichiers en markdown à convertir en fichier html')
+parser.add_argument('-o','--output', type = str, required = True, metavar = '', help='dossier où seront mis les fichiers html créé pour les afficher sur le site site statique')
+args = parser.parse_args()
+
+
+conversion_md = args.input
+resultat_html = args.output
+
+dossier = os.listdir(conversion_md)
+
+
+def converter(fichier_md, resultat_html) :
+    compteur = 0
+    for fichiers in dossier:
+        with open (f'{conversion_md}/{fichiers}',"r") as file :
+            convert_html = markdown2.markdown(file.read())
+            resultat_html = open(f'{resultat_html}/index{compteur}.html',"w")
+            resultat_html.write(convert_html)
+            resultat_html.close()
+            compteur += 1
+
+converter(conversion_md, resultat_html)
